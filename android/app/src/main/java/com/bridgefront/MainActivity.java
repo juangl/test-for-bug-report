e9299ef5408602cb3a569f9486941daed8565933
Juan Jesus Garcia <juanjegarcialopez@gmail.com>
2017-01-18 09:15:02 -0600

    changes

--- android/app/src/main/java/com/bridgefront/MainActivity.java
+++ android/app/src/main/java/com/bridgefront/MainActivity.java
@@ -2,9 +2,14 @@ package com.bridgefront;
 
 import android.content.Intent;
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
 
 public class MainActivity extends ReactActivity {
 
+    CallbackManager mCallbackManager = 
+        MainApplication.getCallbackManager();
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -17,6 +22,6 @@ public class MainActivity extends ReactActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
